6a6006955915130a11042b4bc8fdbeb0492efd2d
staceyko <ko.stacey@gmail.com>
2015-08-30 01:23:49 -0400

    done

--- app.rb
+++ app.rb
@@ -1,5 +1,17 @@
 require 'sinatra/base'
-
+require 'pry'
 class App < Sinatra::Base
 
+  get '/' do
+    erb :super_hero
+  end
+
+  # get '/team' do
+  #   erb :team
+  # end
+
+  post '/teams' do
+    @team = params["team"]
+    # binding.pry
+  end
 end
